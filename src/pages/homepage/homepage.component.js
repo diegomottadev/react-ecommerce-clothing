2dff68502cc97f57819b5032e4c389bb79c68a10
diegomottadev <dmotta@farmakd.com>
2021-02-01 18:21:39 -0300

    create component to homepage and add  more styles

---
+++ src/pages/homepage/homepage.component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Directory from '../../componets/directory/directory';
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className="homepage">
+        <Directory/>
+    </div>
+)
+
+
+export default HomePage;
