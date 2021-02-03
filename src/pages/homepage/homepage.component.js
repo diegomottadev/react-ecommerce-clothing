0a77328a0bad50faef5fc87fe2aae06ff30ed4c4
diegomottadev <dmotta@farmakd.com>
2021-02-03 13:03:23 -0300

    broken our homepage into smaller component finishing styling our menu-item

--- src/pages/homepage/homepage.component.js
+++ src/pages/homepage/homepage.component.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import Directory from '../../componets/directory/directory';
-import './homepage.styles.scss';
+import React from 'react'
+import Directory from '../../componets/directory/directory'
+import './homepage.styles.scss'
 
 const HomePage = () => (
-    <div className="homepage">
-        <Directory/>
-    </div>
+  <div className='homepage'>
+    <Directory />
+  </div>
 )
 
-
-export default HomePage;
+export default HomePage
